4fdb310d98b7a6f1ca5dfd47ae27e463a5d01350
DouSTN <67627088+DouSTN@users.noreply.github.com>
2022-01-07 09:05:34 +0800

    Create main-json.py

---
+++ main-json.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import simplejson as json
+import os
+import time
+
+with open('*json文件存放绝对路径*','r',encoding='utf8')as fp:
+    json_data = json.load(fp)
+
+ti = {'scoreTime':'0'}
+
+while json_data == ti:
+    time.sleep(5)
+    with open('*json文件存放绝对路径*', 'r', encoding='utf8') as fp:
+        json_data = json.load(fp)
+    print ("当前时间：%s" % time.ctime())
+    print ('还未检测到国考成绩发布')
+    print (' ')
+
+
+print('国考成绩已发布，正在发送短信通知！')
+os.system('/usr/bin/python *短信发送脚本存放绝对路径*')
+time.sleep(7200)
